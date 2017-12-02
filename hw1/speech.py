f17692bf1cd16e87e18679e284992a7d9cce1923
New-bottle <775712558@qq.com>
2017-12-03 01:15:42 +0800

    short time energy & zcr

--- hw1/speech.py
+++ hw1/speech.py
@@ -1,10 +1,12 @@
 from Tkinter import *
 import wave
+import math
 import matplotlib.pyplot as plt
 import numpy as np
 
-wav1 = '2.wav'
-threshold = 5000
+wav1 = '1.wav'
+frameSize = 256
+overLap = 128
 def read_wave_data(file_path):
 	# open a wave file, and return a Wave_read object
 	f = wave.open(file_path, 'rb')
@@ -30,7 +32,30 @@ def read_wave_data(file_path):
 	time = np.arange(0, nframes) * (1.0 / framerate)
 	return wave_data, time
 
-len = 10
+def short_time_energy(waveData, frameSize, overLap):
+	wlen = len(waveData)
+	step = frameSize - overLap
+	frameNum = int(math.ceil(wlen*1.0/step))
+	energy = np.zeros((frameNum,1))
+	for i in range(frameNum):
+		curFrame = waveData[np.arange(i*step, min(i*step+frameSize, wlen))]
+		curFrame = curFrame - np.median(curFrame) # zero-justified
+		energy[i] = np.sum(curFrame*curFrame)
+	return energy
+
+def zero_cross_rate(waveData, frameSize, overLap):
+	wlen = len(waveData) - 1
+	step = frameSize - overLap
+	frameNum = int(math.ceil(wlen*1.0/step))
+	crossRate = np.zeros((frameNum,1))
+	for i in range(frameNum):
+		curFrame = waveData[np.arange(i*step, min(i*step+frameSize, wlen))]
+		curFrame = curFrame - np.median(curFrame) # zero-justified
+		for j in range(len(curFrame) - 1):
+			if (curFrame[j] * curFrame[j + 1] <= 0):
+				crossRate[i] += 1
+	return crossRate
+
 def find_boundary(time, wave_data):
 	bound_data = ['k'] * time.shape[0]
 	for i in range(time.shape[0] - len):
@@ -46,16 +71,21 @@ def find_boundary(time, wave_data):
 	return bound_data
 
 def main():
-	wave_data, time = read_wave_data(wav1)
-	bound_data = find_boundary(time, wave_data)
+	waveData, time = read_wave_data(wav1)
 	#draw the wave
 	plt.subplot(311)
-	#print bound_data
-	plt.scatter(time, wave_data[0], s=5, c=bound_data)
+	plt.plot(time, waveData[0], c = 'r')
+
 	plt.subplot(312)
-	plt.plot(time, wave_data[1], c = "g")
+	energy = short_time_energy(waveData[0], frameSize, overLap)
+	time2 = time[0:len(energy)]
+	plt.plot(time2, energy, c = "b")
+	print energy
+
 	plt.subplot(313)
-	plt.plot(np.fft.fft(wave_data[0]))
+	zcr = zero_cross_rate(waveData[0], frameSize, overLap)
+	plt.plot(time2, zcr, c = "g")
+	plt.plot()
 	plt.show()
 
 if __name__ == "__main__":
